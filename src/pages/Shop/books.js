c6722b5f4f70cc17696ee273dac48ddd1853ef49
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2024-04-19 15:18:12 +0500

    update

---
+++ src/pages/Shop/books.js
@@ -0,0 +1,11 @@
+//  tarixiy, badiiy , ilmiy , diniy ,
+export const bookData = [
+    {
+        id: 1,
+        name: "Mehrobdan Chayon",
+        img: "https://assets.asaxiy.uz/product/items/desktop/d290dc6cabaffa37f5473eb33611607e2022080311593712870YwY9yiATH0.jpg",  
+        price: "30 000",
+    }
+]
+
+// 48-63
