66391d9aeb399f35431fe002e42eb7ac77fa2b24
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2024-04-19 20:51:42 +0500

    update-data

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import banner from '../../img/shopbanner.png'
-
+import './Shop.scss'
 function Shop() {
   return (
-    <div>
-        <Hero path={'Shop'} title={`Eduvi Online Book Shop`} banner={banner}/>
-        <h1>Shop</h1>
+    <div className='shops'>
+        <Hero  path={'Shop'} title={`Eduvi Online Book Shop`} banner={banner} />
+        <h1 className=''>Shop</h1>
+
     </div>
   )
 }
--- src/pages/Shop/books.js
+++ src/pages/Shop/books.js
@@ -2,10 +2,183 @@
 export const bookData = [
     {
         id: 1,
-        name: "Mehrobdan Chayon",
-        img: "https://assets.asaxiy.uz/product/items/desktop/d290dc6cabaffa37f5473eb33611607e2022080311593712870YwY9yiATH0.jpg",  
-        price: "30 000",
-    }
+        name: "To Kill a Mockingbird",
+        price: 12.99,
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg"
+      },
+      {
+        id: 2,
+        name: "1984",
+        price: 10.49,
+        image: "https://m.media-amazon.com/images/I/71rpa1-kyvL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 3,
+        name: "The Great Gatsby",
+        price: 11.99,
+        image: "https://m.media-amazon.com/images/I/81QuEGw8VPL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 4,
+        name: "The Catcher in the Rye",
+        price: 9.99,
+        image: "https://cdn.britannica.com/94/181394-050-2F76F7EE/Reproduction-cover-edition-The-Catcher-in-the.jpg"
+      },
+      {
+        id: 5,
+        name: "Brave New World",
+        price: 10.99,
+        image: "https://m.media-amazon.com/images/I/81zE42gT3xL._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+      },
+      {
+        id: 6,
+        name: "Lord of the Flies",
+        price: 8.49,
+        image: "https://m.media-amazon.com/images/I/81WUAoL-wFL._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+      },
+      {
+        id: 7,
+        name: "Animal Farm",
+        price: 7.99,
+        image: "https://m.media-amazon.com/images/I/81WoYpcR34L._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 8,
+        name: "Pride and Prejudice",
+        price: 13.49,
+        image: "https://m.media-amazon.com/images/M/MV5BMTA1NDQ3NTcyOTNeQTJeQWpwZ15BbWU3MDA0MzA4MzE@._V1_.jpg"
+      },
+      {
+        id: 9,
+        name: "The Hobbit",
+        price: 14.99,
+        image: "https://m.media-amazon.com/images/I/712cDO7d73L._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 10,
+        name: "Harry Potter and the Philosopher's Stone",
+        price: 15.99,
+        image: "https://m.media-amazon.com/images/I/81q77Q39nEL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 11,
+        name: "The Lord of the Rings",
+        price: 22.99,
+        image: "https://m.media-amazon.com/images/I/7125+5E40JL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 12,
+        name: "The Chronicles of Narnia",
+        price: 19.99,
+        image: "https://m.media-amazon.com/images/I/81RuC2qCSmL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 13,
+        name: "The Hitchhiker's Guide to the Galaxy",
+        price: 11.49,
+        image: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/25/1435245979235/047c9878-9845-473c-9635-5f32545746b0-1355x2040.jpeg?width=700&quality=85&auto=format&fit=max&s=606433bda33c8c27c5ebd7ba85900473"
+      },
+      {
+        id: 14,
+        name: "Moby-Dick",
+        price: 16.99,
+        image: "https://m.media-amazon.com/images/I/71d5wo+-MuL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 15,
+        name: "Frankenstein",
+        price: 9.99,
+        image: "https://m.media-amazon.com/images/I/81TVqiv-ctL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 16,
+        name: "Wuthering Heights",
+        price: 11.49,
+        image: "https://m.media-amazon.com/images/I/61Ap7QjGMjL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 17,
+        name: "Dracula",
+        price: 10.99,
+        image: "https://m.media-amazon.com/images/I/61TqVMQ6jKL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 18,
+        name: "The Picture of Dorian Gray",
+        price: 8.99,
+        image: "https://m.media-amazon.com/images/I/51IPe62r8gL._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+      },
+      {
+        id: 19,
+        name: "Jane Eyre",
+        price: 12.99,
+        image: "https://m.media-amazon.com/images/I/91zU70Aw9IS._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 20,
+        name: "Gone with the Wind",
+        price: 13.99,
+        image: "https://m.media-amazon.com/images/I/91w1IHrUqZL._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+      },
+      {
+        id: 21,
+        name: "The Road",
+        price: 9.49,
+        image: "https://m.media-amazon.com/images/I/51M7XGLQTBL._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+      },
+      {
+        id: 22,
+        name: "The Grapes of Wrath",
+        price: 10.99,
+        image: "https://m.media-amazon.com/images/I/814oeXzxhML._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 23,
+        name: "The Old Man and the Sea",
+        price: 8.99,
+        image: "https://m.media-amazon.com/images/I/71RXc0OoEwL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 24,
+        name: "One Hundred Years of Solitude",
+        price: 11.99,
+        image: "https://m.media-amazon.com/images/I/81MI6+TpYkL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 25,
+        name: "Catch-22",
+        price: 11.49,
+        image: "https://m.media-amazon.com/images/I/71Ym0vDDWsL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 26,
+        name: "The Stranger",
+        price: 9.99,
+        image: "https://m.media-amazon.com/images/I/81GjCVSEDAL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 27,
+        name: "Slaughterhouse-Five",
+        price: 10.49,
+        image: "https://m.media-amazon.com/images/I/51gi6rxS6OL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 28,
+        name: "The Bell Jar",
+        price: 8.99,
+        image: "https://m.media-amazon.com/images/I/81wUVpREPSL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 29,
+        name: "The Count of Monte Cristo",
+        price: 14.49,
+        image: "https://m.media-amazon.com/images/I/51RcuPqc+lL._AC_UF1000,1000_QL80_.jpg"
+      },
+      {
+        id: 30,
+        name: "Anna Karenina",
+        price: 12.49,
+        image: "https://m.media-amazon.com/images/I/91F9WNEThJL._AC_UF1000,1000_QL80_.jpg"
+      },
 ]
 
-// 48-63
