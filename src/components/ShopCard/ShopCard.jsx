a56c9e80755e52ba354ed7e553ccf875bd0996f8
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2024-04-21 17:06:02 +0500

    shop-update

---
+++ src/components/ShopCard/ShopCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './ShopCard.scss'
+import { Rate } from 'antd';
+function ShopCard({item}) {
+
+  return (
+    <div className='shop__card'>
+    <div className='card__img__box'>
+    <img className='shop__card__img' src={item.image} alt="" />
+    </div>
+    <h1>{item.title}</h1>
+    <div className='shop__card__ul'>
+    <li className='shop__card__ul__li'>{item.price}</li>
+    <li className='shop__card__ul__li'><Rate disabled defaultValue={item.rating} /></li>
+    
+    </div>
+    </div>
+  )
+}
+
+export default ShopCard
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 import Hero from '../../components/Hero/Hero'
 import banner from '../../img/shopbanner.png'
 import './Shop.scss'
+import ShopCard from '../../components/ShopCard/ShopCard'
+import { Pagination } from 'antd';
+import {bookData} from '../../components/Data/books'
 function Shop() {
+  const itemsPerPage = 9; // Number of items to display per page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calculate the start and end index of the items to display for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, bookData.length); // Ensure endIndex doesn't exceed bookData.length
+
+  // Slice the bookData array to get the items for the current page
+  const currentPageData = bookData.slice(startIndex, endIndex);
+
+  // Handler function to change the current page
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
     <div className='shops'>
         <Hero  path={'Shop'} title={`Eduvi Online Book Shop`} banner={banner} />
-        <h1 className=''>Shop</h1>
+        <div className='fatherShop'>
+        <div className="container ">
+          <section className='shopDivider'>
+          <div className='leftShop'></div>
+<div className='rightShop'>
+
+<div className='Shop__list'>
+
+{
+                  currentPageData.map((item, index) => (
+                    <ShopCard key={index} item={item} />
+                  ))
+                }
+                <Pagination className='pagiShop'
+                current={currentPage}
+                pageSize={itemsPerPage}
+                total={bookData.length}
+                onChange={handlePageChange}
+              />
+</div>
+
+</div>
+          </section>
+        </div>
+        </div>
 
     </div>
   )
 }
+
+
 
 export default Shop
