ef562c4973def7c0d33f871677a0f0ccfe18232e
oldboy16 <hamidullayevmuhammadamin95gmail.com>
2024-04-22 14:27:50 +0500

    sdk

---
+++ src/components/Standard/Standard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Standard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Standard
--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Courses.scss'
 import Hero from '../../components/Hero/Hero'
 import banner from '../../img/shopbanner.png'
+import Standard from '../../components/Standard/Standard'
 function Courses() {
   return (
     <div>
       <Hero path={'Courses'} title={`Eduvi Courses For All Standards`} banner={banner}/>
-        <h1>Courses</h1>
+      <Standard/>
     </div>
   )
 }
